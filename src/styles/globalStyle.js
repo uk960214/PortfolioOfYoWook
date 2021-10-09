5bf3be72ab5690e0afd8c96576abc59276b2806b
koowoy <uk960214@gmail.com>
2021-10-09 23:08:29 +0900

    images collected

--- src/styles/globalStyle.js
+++ src/styles/globalStyle.js
@@ -1,5 +1,5 @@
 import { createGlobalStyle } from "styled-components";
-import background from "../image1.jpg";
+import background from "../images/image1.jpg";
 
 const GlobalStyle = createGlobalStyle`
 *{
@@ -9,8 +9,6 @@ const GlobalStyle = createGlobalStyle`
 }
 
 body {
-  @import url('https://fonts.googleapis.com/css2?family=Maven+Pro:wght@700&display=swap');
-  @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@300;500&family=Satisfy&display=swap');
   background: linear-gradient(0deg, rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0.3)), url(${background}) center center/cover;
   
 }
